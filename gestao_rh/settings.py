53c997ec8656d40045a6b99ce36e94cc1796f8f2
wellington-costa <wellington_costa@live.com>
2019-04-02 21:10:45 -0300

    conf. STATIC_ROOT

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -140,7 +140,7 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home'
 
